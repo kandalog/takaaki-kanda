c186b5d36b1ca8cadcda426c2b3fb1abe14ed3ce
kandalog <asuasu.tee@gmail.com>
2025-03-19 20:05:40 +0900

    refactor: ディレクトリ構成の変更
    共通コンポーネントをラップしたページ固有のコンポーネントの置き場所を作るため

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import Header from "./components/Header";
+import Header from "./components/common/Header";
+import MainContainer from "./components/common/MainContainer";
+
+import AboutSection from "./components/presentation/home/AboutSection";
+import SkillSection from "./components/presentation/home/SkillSection";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <MainContainer>
+        <AboutSection />
+        <SkillSection />
+      </MainContainer>
+    </>
   );
 }
 
---
+++ src/components/presentation/home/SkillSection.tsx
@@ -0,0 +1,20 @@
+import Section from "../../common/Section";
+
+function SkillSection() {
+  return (
+    <Section divider title="スキル">
+      <p>
+        <strong>プログラミング言語:</strong> TypeScript, Ruby, Python
+      </p>
+      <p>
+        <strong>フレームワーク & ライブラリ:</strong> React, Next.js, Rails,
+        Vitest, RSpec
+      </p>
+      <p>
+        <strong>ツール:</strong> Git, Linux, Docker, Sentry, Metabase
+      </p>
+    </Section>
+  );
+}
+
+export default SkillSection;
