12c72da40ec89c8e40c0228f639f642e22abdbf6
kandalog <asuasu.tee@gmail.com>
2025-03-20 01:49:17 +0900

    update: sns link追加

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,18 +5,30 @@ export function Footer() {
       <p className="footer-text">Built React.</p>
       <ul className="sns-link-list">
         <li className="sns-link-item">
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <img src="/src/assets/github.png" alt="" />
+          <a
+            href="https://github.com/kandalog"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/src/assets/github.png" alt="github" />
           </a>
         </li>
         <li className="sns-link-item">
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <img src="/src/assets/qiita.png" alt="" />
+          <a
+            href="https://qiita.com/kandalog"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/src/assets/qiita.png" alt="qiita" />
           </a>
         </li>
         <li className="sns-link-item">
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <img src="/src/assets/x.png" alt="" />
+          <a
+            href="https://x.com/tee_develop"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/src/assets/x.png" alt="x" />
           </a>
         </li>
       </ul>
